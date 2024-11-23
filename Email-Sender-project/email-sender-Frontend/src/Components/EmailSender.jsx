c95052f4b13d7eb5f60d11d7882e084ccc63a43e
Deepak Negi <deepaknegi@Deepaks-MacBook-Air.local>
2024-11-23 19:59:51 +0530

    final changes

--- Email-Sender-project/email-sender-Frontend/src/Components/EmailSender.jsx
+++ Email-Sender-project/email-sender-Frontend/src/Components/EmailSender.jsx
@@ -168,6 +168,8 @@ function EmailSender() {
                 ],
                 toolbar:
                   "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                  height:300,
+                  width:"100%",
                 tinycomments_mode: "embedded",
                 tinycomments_author: "Author name",
                 mergetags_list: [
