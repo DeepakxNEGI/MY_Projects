9b538b113f2e5f7d54c856aa7273fa00af3e5bac
DeepakxNEGI <130681263+DeepakxNEGI@users.noreply.github.com>
2024-12-13 15:09:25 +0530

    .env file added

--- Email-Sender-project/email-sender-Frontend/src/service/helper.js
+++ Email-Sender-project/email-sender-Frontend/src/service/helper.js
@@ -1,6 +1,16 @@
-import axios from "axios";
-const baseURL='http://localhost:8080/api/v1'
-export const  customAxios= axios.create({
+// import axios from "axios";
+// const baseURL='http://localhost:8080/api/v1'
+// export const  customAxios= axios.create({
 
-    baseURL:baseURL,
+//    baseURL:baseURL,
+// });
+
+
+import axios from "axios";
+
+// Use the REACT_APP_API_URL from the .env files
+const baseURL = process.env.REACT_APP_API_URL;
+
+export const customAxios = axios.create({
+   baseURL: baseURL,
 });
