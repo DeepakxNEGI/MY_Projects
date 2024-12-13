1efff9acd96f0c72ae372db94241fbf8ae27867e
DeepakxNEGI <130681263+DeepakxNEGI@users.noreply.github.com>
2024-12-13 15:34:37 +0530

    code without env file

--- Email-Sender-project/email-sender-Frontend/src/service/helper.js
+++ Email-Sender-project/email-sender-Frontend/src/service/helper.js
@@ -1,16 +1,30 @@
 // import axios from "axios";
+// // const baseURL='http://localhost:8080/api/v1'
 // const baseURL='http://localhost:8080/api/v1'
+
 // export const  customAxios= axios.create({
 
 //    baseURL:baseURL,
 // });
 
 
+// import axios from "axios";
+
+// // Use the REACT_APP_API_URL from the .env files
+// const baseURL = process.env.REACT_APP_API_URL;
+
+// export const customAxios = axios.create({
+//    baseURL: baseURL,
+// });
+
+
 import axios from "axios";
 
-// Use the REACT_APP_API_URL from the .env files
-const baseURL = process.env.REACT_APP_API_URL;
+// Check if we are running in a production environment (Render)
+const baseURL = window.location.hostname === 'email-frontend-rd2z.onrender.com'
+    ? 'https://my-projects-oism.onrender.com/api/v1'  // Render URL
+    : 'http://localhost:8080/api/v1';  // Localhost URL for local development
 
 export const customAxios = axios.create({
-   baseURL: baseURL,
+    baseURL: baseURL,
 });
